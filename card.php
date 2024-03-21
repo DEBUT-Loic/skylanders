768883236aecca46c63eb119ddbcac158ea7b114
DEBUT Loïc <94994920+DEBUT-Loic@users.noreply.github.com>
2024-03-21 19:28:46 +0100

    Add files via upload

---
+++ card.php
@@ -0,0 +1,55 @@
+<?php
+$path = 'skylandersGiants.json';
+$jsonString = file_get_contents($path);
+$jsonData = json_decode($jsonString, true);
+asort($jsonData);
+$jsonData=array_values($jsonData);
+
+
+if(!empty($_GET["stats"])) {
+    function compareStats($a, $b) {
+        if ($a[$_GET['stats']] == $b[$_GET['stats']]) {
+            return 0;
+        }
+        return ($a[$_GET['stats']] > $b[$_GET['stats']]) ? -1 : 1;
+    }
+    usort($jsonData, 'compareStats');
+}
+
+
+$_GET['stats']="";
+if(!empty($_GET['element']) || !empty($_GET['version']) || !empty($_GET['special'])) {
+    $_GET=array_filter($_GET);
+    
+    foreach($jsonData as $keyJson=>$valJson) {
+        foreach($_GET as $keyGet=>$valGet) {
+            if(isset($valJson)) {
+                if(($valJson[$keyGet]!=$valGet && $keyGet!="special") || ($valGet=="Base" && $valJson[$keyGet]!==null) || ($valGet=="Dérive" && $valJson[$keyGet]===null) || ($valGet=="Legendary" && $valJson[$keyGet]!="Legendary")) {
+                    unset($jsonData[$keyJson]);
+                }
+            }
+        }
+    }
+}
+
+foreach($jsonData as $val) {
+    ?>
+    <article class="<?= strtolower($val["element"]); ?> <?= $val["special"]=="Legendary" ? strtolower($val["special"]) : ""; ?>">
+        <h1><?= $val["special"]!="" ? $val["special"]." ".$val["nom"] : ($val["version"]=="Lightcore" ? $val["version"]." ".$val["nom"] : $val["nom"]); ?></h1>
+        <div class="imgCard">
+            <img src="img/Giants/<?= $val["special"]!="" ? $val["nom"]."_".$val["special"] : ($val["version"]=="Lightcore" ? $val["nom"]."_".$val["version"] : $val["nom"]); ?>.webp" alt="<?= $val["nom"]; ?>">
+            <small><?= $val["version"] ?></small>
+        </div>
+        <div class="stats">
+            <ul>
+                <li><b>Santé</b> : <?= $val["sante"]; ?></li>
+                <li><b>Vitesse</b> : <?= $val["vitesse"]; ?></li>
+                <li><b>Armure</b> : <?= $val["armure"]; ?></li>
+                <li><b>Coup critique</b> : <?= $val["coup_critique"]; ?></li>
+                <li><b>Puissance élémentaire</b> : <?= $val["puissance_elementaire"]; ?></li>
+            </ul>
+        </div>
+    </article>
+    <?php
+}
+?>
